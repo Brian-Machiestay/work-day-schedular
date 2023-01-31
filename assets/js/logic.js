cb906d9c181c2040d2368734625ddff03c7c67b9
Brian-Machiestay <brianmachiestay@gmail.com>
2023-01-31 11:53:24 +0000

    create a function for saving events

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,9 +1,41 @@
 // Get the current date and time
 let now = moment();
-dayNow = now.format('dddd');
-mthNow = now.format('MMMM');
-dayNum = now.format('Do');
-console.log(dayNow);
+date = now.format('dddd, MMMM Do');
 
-// populate the DOM with the date and time
-$("#currentDay").text(`${dayNow}, ${mthNow} ${dayNum}`);
+// populate the DOM with the current date
+$("#currentDay").text(date);
+
+
+// write a function to save user events
+function save(event) {
+    // save events entered by the user
+    let events = [];
+    // get the time of logged event
+    const hour = event.currentTarget.parentNode.children[0].textContent;
+    // get the user entered event
+    const userEvent = event.currentTarget.parentNode.children[1].textContent;
+    // get stored item
+    let storedEvents = localStorage.getItem('events');
+    ob = {
+        hour: hour.split(' ')[0],
+        userEvent: userEvent,
+    };
+    
+    if (storedEvents) {
+        storedEvents = JSON.parse(storedEvents);
+        storedEvents.push(ob);
+        events = storedEvents;
+        localStorage.setItem('events', storedEvents);
+    }
+    else {
+        events.push(ob);
+        localStorage.setItem('events', JSON.stringify(events));
+    }
+}
+
+if (localStorage.getItem('events')) {
+    const allEvents = JSON.parse(localStorage.getItem('events'))
+}
+
+// attach a listener to all save buttons
+$(".saveBtn").on("click", save);
