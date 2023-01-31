1b7016c992cf5ddfbbfa3fbc162c2b0e55f1cf99
Brian-Machiestay <brianmachiestay@gmail.com>
2023-01-31 17:49:49 +0000

    persist user data in storage

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,42 @@
+let eventsHour = [
+    {
+        hour9: 9,
+        event: '',
+    },
+    {
+        hour10: 10,
+        event: '',
+    },
+    {
+        hour11: 11,
+        event: '',
+    },
+    {
+        hour12: 12,
+        event: '',
+    },
+    {
+        hour1: 1,
+        event: '',
+    },
+    {
+        hour2: 2,
+        event: '',
+    },
+    {
+        hour3: 3,
+        event: '',
+    },
+    {
+        hour4: 4,
+        event: '',
+    },
+    {
+        hour5: 5,
+        event: '',
+    }
+]
+
 // Get the current date and time
 let now = moment();
 date = now.format('dddd, MMMM Do');
@@ -8,33 +47,37 @@ $("#currentDay").text(date);
 
 // write a function to save user events
 function save(event) {
-    // save events entered by the user
-    let events = [];
-    // get the time of logged event
-    const hour = event.currentTarget.parentNode.children[0].textContent;
     // get the user entered event
-    const userEvent = event.currentTarget.parentNode.children[1].textContent;
+    const userEvent = event.currentTarget.parentNode.children[1].value;
     // get stored item
+    let hours = 9;
     let storedEvents = localStorage.getItem('events');
-    ob = {
-        hour: hour.split(' ')[0],
-        userEvent: userEvent,
-    };
-    
     if (storedEvents) {
-        storedEvents = JSON.parse(storedEvents);
-        storedEvents.push(ob);
-        events = storedEvents;
-        localStorage.setItem('events', storedEvents);
+        eventsHour = JSON.parse(storedEvents);
+        localStorage.removeItem('events');
     }
-    else {
-        events.push(ob);
-        localStorage.setItem('events', JSON.stringify(events));
+    for (let i = 0; i < eventsHour.length; i++) {
+        // get the time of logged event
+        const hour = event.currentTarget.parentNode.children[0].textContent.split(' ')[0];
+        if (hour == hours) {
+            eventsHour[i].event = userEvent;
+        }
+        hours++;
+        if (hours > 12) hours = hours - 12;
     }
+    localStorage.setItem('events', JSON.stringify(eventsHour));
 }
 
+// onload events from storage
 if (localStorage.getItem('events')) {
-    const allEvents = JSON.parse(localStorage.getItem('events'))
+    eventsHour = JSON.parse(localStorage.getItem('events'));
+}
+let hr = 9;
+for (let j = 0; j < eventsHour.length; j++) {
+    console.log(eventsHour[j]);
+    $(`.hour${hr}`).siblings(".textarea").val(eventsHour[j].event);
+    hr++;
+    if (hr > 12) hr = hr - 12;
 }
 
 // attach a listener to all save buttons
