642004ed71a07b634bbb2cb5252d4f9728459eb3
Brian-Machiestay <brianmachiestay@gmail.com>
2023-01-31 05:11:25 +0000

    render the current date

--- js/logic.js
+++ js/logic.js
@@ -1,2 +1,9 @@
+// Get the current date and time
 let now = moment();
-console.log(now.toJSON.prototype);
+dayNow = now.format('dddd');
+mthNow = now.format('MMMM');
+dayNum = now.format('Do');
+console.log(dayNow);
+
+// populate the DOM with the date and time
+$("#currentDay").text(`${dayNow}, ${mthNow} ${dayNum}`);
